53f2001a3f8a8d4850a705fdf7855bf239234b07
stevannohero <stevannohero@gmail.com>
2018-05-03 13:33:45 +0700

    fix navbar

--- styles/animated.js
+++ styles/animated.js
@@ -9,3 +9,10 @@ $(function () {
 		event.preventDefault();
 	});
 });
+
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".navbar-fixed-top");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
