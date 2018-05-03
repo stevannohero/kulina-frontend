9629565c0c91548118e3c8581444b5a9c4f35c3b
stevannohero <stevannohero@gmail.com>
2018-05-03 09:22:21 +0700

    add header

---
+++ styles/animated.js
@@ -0,0 +1,11 @@
+$(function () {
+	"use strict";
+
+	$('a.page-scroll').bind('click', function (event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top - 68
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
